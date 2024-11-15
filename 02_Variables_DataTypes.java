63552176aeca19009f2a6f55ee102e2fa5ae729f
Abhik <chatterjeeabhik94@gmail.com>
2024-11-15 13:11:58 +0530

    Add files via upload

---
+++ 02_Variables_DataTypes.java
@@ -0,0 +1,30 @@
+//Rules for declaring a variable name:-
+//1. Must not begin with a digit
+//2. Blank spaces are not allowed
+//3. Name can't be a reserved keyword
+//4. Can contain characters like $ _
+//5. Variable name is case sensitive
+
+class VariableClass
+{
+	public static void main(String[] args)
+	{
+		int i=7;
+		System.out.println("Value: "+ i);
+		
+		long l=33333333;
+		System.out.println("Value: "+ l);
+		
+		float f=3.14f;
+		System.out.println("Value: "+ f);
+		
+		double d=98100.00001;
+		System.out.println("Value: "+ d);
+		
+		char c='A';
+		System.out.println("Value: "+ c);
+		
+		String s="Hello";
+		System.out.println("Value: "+ s);
+	}
+}
