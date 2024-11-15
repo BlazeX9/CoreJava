2fb07025ad707aaf8c2e15ce0004046fbe68a923
Abhik <chatterjeeabhik94@gmail.com>
2024-11-15 12:46:01 +0530

    00_java.java

--- 00_java.java
+++ 00_java.java
@@ -1,3 +1,12 @@
+Java is an object oriented programming language
+
+Java is compiled into the bytecode and then its interpreted into machine code
+[Source code] --Compiled--> [Byte code] --Interpreted--> [Machine code]
+
+
+
+
+
 class MyClass
 {
 	public static void main(String[] args)
@@ -7,5 +16,5 @@ class MyClass
 }
 
 
-To complie > javac filename.javac (javac 00_java.java)
-To run > java className (java MyClass)
+complie --> javac filename.javac (javac 00_java.java)
+run --> java className (java MyClass)
