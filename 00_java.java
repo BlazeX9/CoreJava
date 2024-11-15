5e5f0129fd39a1a499e450dc6fc9412524826817
Abhik <chatterjeeabhik94@gmail.com>
2024-11-15 12:42:20 +0530

    Add files via upload

---
+++ 00_java.java
@@ -0,0 +1,11 @@
+class MyClass
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Hello world");
+	}
+}
+
+
+To complie > javac filename.javac (javac 00_java.java)
+To run > java className (java MyClass)
