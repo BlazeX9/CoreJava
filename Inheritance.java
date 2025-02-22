2b057e8aa128b9dfb4807f8c85db4c2d414e9e52
Abhik <chatterjeeabhik94@gmail.com>
2025-02-23 01:56:49 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,75 @@
+//1. Simple Inheritance  [P] --> [C]
+import java.util.Scanner;
+class MyClass
+{
+	public static void main(String[] args)
+	{
+		classChild obj = new classChild();
+		obj.input();
+		obj.show();
+	}
+}
+
+class classParent
+{
+	String n,c;
+	int r;
+	void input()
+	{
+	Scanner MyScn1 = new Scanner(System.in);
+	System.out.print("Enter name ");
+	n = MyScn1.nextLine();
+	System.out.print("Enter course ");
+	c = MyScn1.nextLine();
+	
+	Scanner MyScn2 = new Scanner(System.in);
+	System.out.print("Enter roll ");
+	r = MyScn2.nextInt();
+	}
+}
+
+class classChild extends classParent
+{
+	void show()
+	{
+		System.out.print("User "+n+" has selected course "+c+" and his roll number is "+r);
+	}
+}
+
+//2. Multilevel Inheritance  [GP] --> [P] --> [C]
+class MyClass
+{
+	public static void main(String[] args)
+	{
+		classChild obj = new classChild();
+		obj.value(5);
+		obj.newvalue();
+		obj.show();
+	}
+}
+
+class grandClass
+{
+	int x;
+	void value(int a)
+	{
+		x=a;
+	}
+}
+
+class parentClass extends grandClass
+{
+	int y;
+	void newvalue()
+	{
+		y=x+5;
+	}
+}
+
+class classChild extends parentClass
+{
+	void show()
+	{
+		System.out.print("U "+y);
+	}
+}
