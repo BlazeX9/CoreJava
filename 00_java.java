825f3c1eac78f719ec7f9e7851b9fedf5fd6fa2c
Abhik <chatterjeeabhik94@gmail.com>
2025-03-19 13:23:38 +0530

    -

--- 00_java.java
+++ 00_java.java
@@ -4,17 +4,23 @@ Java is compiled into the bytecode and then its interpreted into machine code
 [Source code] --Compiled--> [Byte code] --Interpreted--> [Machine code]
 
 
-
-
-
 class MyClass
 {
 	public static void main(String[] args)
 	{
-		System.out.print("Hello world");
+	 System.out.print("Hello world");
 	}
 }
 
 
-complie --> javac filename.javac (javac 00_java.java)
-run --> java className (java MyClass)
+complie --> javac filename.javac
+run --> java className
+
+	
+++a (pre-increment) --> Increments the value of a by 1 and returns the new value of a after incrementing
+	int a=5;
+        int b=++a;  --> a becomes 6 and b becomes 6
+
+a++ (post-increment) --> Increments the value of a by 1 and returns the original value of a before incrementing
+        int a=5;
+        int b=a++;   --> a becomes 6 and b becomes 5
