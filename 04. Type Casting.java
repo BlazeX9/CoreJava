af6a9a9f12d3c1f5b0f383f38c68f671cb9fd8ab
Abhik <chatterjeeabhik94@gmail.com>
2025-04-20 10:32:52 +0530

    +

--- 04. Type Casting.java
+++ 04. Type Casting.java
@@ -6,7 +6,7 @@ class MyClass
   int num2=6;
   int num3=7;
   int sum=num1+num2+num3;
-  float avg=sum/3;  	         --> Result will be in intiger
+  float avg=sum/3;            --> Result will be in intiger
   float avg=(float)sum/3;     --> Result will be in float
   float avg=sum/(float)3;     --> Result will be in float
   float avg=sum/3f;           --> Result will be in float
