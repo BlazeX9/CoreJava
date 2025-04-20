797345a230fb8c171fd84846083f537119984569
Abhik <chatterjeeabhik94@gmail.com>
2025-04-20 10:07:09 +0530

    +

--- 02. Variables & DataTypes.java
+++ 02. Variables & DataTypes.java
@@ -1,30 +1,30 @@
-//Rules for declaring a variable name:-
-//1. Must not begin with a digit
-//2. Blank spaces are not allowed
-//3. Name can't be a reserved keyword
-//4. Can contain characters like $ _
-//5. Variable name is case sensitive
+[Notes] Rules for declaring a variable name
+1. Must not begin with a digit
+2. Blank spaces are not allowed
+3. Name can't be a reserved keyword
+4. Can contain characters like $ _
+5. Variable name is case sensitive
 
-class VariableClass
+class MyClass
 {
-	public static void main(String[] args)
-	{
-		int i=100;
-		System.out.println("Value: "+ i);
+ public static void main(String[] args)
+ {
+  int i=100;
+  System.out.println("Value: "+ i);
 		
-		long l=33333333;
-		System.out.println("Value: "+ l);
+  long l=33333333;
+  System.out.println("Value: "+ l);
 		
-		float f=3.14f;
-		System.out.println("Value: "+ f);
+  float f=3.14f;
+  System.out.println("Value: "+ f);
 		
-		double d=100.53;
-		System.out.println("Value: "+ d);
+  double d=100.53;
+  System.out.println("Value: "+ d);
 		
-		char c='A';
-		System.out.println("Value: "+ c);
+  char c='A';
+  System.out.println("Value: "+ c);
 		
-		String s="Hello";
-		System.out.println("Value: "+ s);
-	}
+  String s="Hello";
+  System.out.println("Value: "+ s);
+ }
 }
