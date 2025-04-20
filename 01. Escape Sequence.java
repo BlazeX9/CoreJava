03297edd0e2ff5bd219dcb8b878aecc3856e9ac3
Abhik <chatterjeeabhik94@gmail.com>
2025-04-20 10:04:29 +0530

    +

--- 01. Escape Sequence.java
+++ 01. Escape Sequence.java
@@ -1,17 +1,20 @@
 class MyClass
 {
-	public static void main(String[] args)
-	{
-	 \t --> space
-	 \n --> next line
-	 \' \' --> ' '
-	 \" \" --> " "
-		
-	 System.out.println("1)\"Hello\"\t\'World\'");  --> 1)"Hello" 'World'
-	 System.out.println("2)\'Hello\'\t\"World\"");  --> 2)'Hello' "World"
-	 System.out.println("3)\"Hello\"\n\'World\'");
-	 System.out.println("4)\'Hello\'\n\"World\"");
-	 System.out.println("5)\"Amity\"\t\'Computar\'\tAcademy");
-	 System.out.println("6)Name\tRoll\tCourse\tMarks\tGrade\nRam\t1\tC\t90\tAA\nShyam\t2\tc++\t80\tA+");
-	}
+ public static void main(String[] args)
+ {		
+  System.out.println("1)\"Hello\"\t\'World\'");  --> 1)"Hello" 'World'
+  System.out.println("2)\'Hello\'\t\"World\"");  --> 2)'Hello' "World"
+  System.out.println("3)\"Hello\"\n\'World\'");
+  System.out.println("4)\'Hello\'\n\"World\"");
+  System.out.println("5)\"Amity\"\t\'Computar\'\tAcademy");
+  System.out.println("6)Name\tRoll\tCourse\tMarks\tGrade\nRam\t1\tC\t90\tAA\nShyam\t2\tc++\t80\tA+");
+ }
 }
+
+
+
+[Notes]
+\t    --> space
+\n    --> next line
+\' \' --> ' '
+\" \" --> " "
