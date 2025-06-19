6ffd3a81a15bcca035936584713300666205d8a1
Abhik <chatterjeeabhik94@gmail.com>
2025-06-19 13:44:33 +0530

    +

--- 02. Variables & DataTypes.java
+++ 02. Variables & DataTypes.java
@@ -1,30 +1,29 @@
-[Notes] Rules for declaring a variable name
+#Rules for declaring a variable name:-
 1. Must not begin with a digit
 2. Blank spaces are not allowed
 3. Name can't be a reserved keyword
 4. Can contain characters like $ _
 5. Variable name is case sensitive
 
-class MyClass
-{
- public static void main(String[] args)
- {
-  int i=100;
-  System.out.println("Value: "+ i);
-		
-  long l=33333333;
-  System.out.println("Value: "+ l);
-		
-  float f=3.14f;
-  System.out.println("Value: "+ f);
-		
-  double d=100.53;
-  System.out.println("Value: "+ d);
-		
-  char c='A';
-  System.out.println("Value: "+ c);
-		
-  String s="Hello";
-  System.out.println("Value: "+ s);
- }
+public class MyClass {
+	public static void main(String[] args) {
+
+	int a = 7;
+	System.out.println(a);
+
+	long b = 1234567890123456789l;	//l
+	System.out.println(b);
+
+	float c = 3.14f;		//f
+	System.out.println(c);
+
+	double d = 3.14159265359;
+	System.out.println(d);
+
+	char e = 'X';
+	System.out.println(e);		//Single quotation
+
+	String f = "Java";
+	System.out.println(f);		//Double quotation
+   }
 }
