55fa3949d75ea712a1c6123bcc7e5af4dcd522ac
Abhik <chatterjeeabhik94@gmail.com>
2025-06-19 14:36:14 +0530

    +

--- 04. Type Casting.java
+++ 04. Type Casting.java
@@ -1,19 +1,18 @@
 public class MyClass {
-	   public static void main(String[] args) {
+    public static void main(String[] args) {
+	int a = 9;
+	double b = a;			//Automatic casting: int to double
+	System.out.println(a);
+	System.out.println(b);
 		
-		int a = 9;
-		double b = a;			//Automatic casting: int to double
-		System.out.println(a);
-		System.out.println(b);
+	double c = 9.78d;
+	int d = (int) c; 		//Manual casting: double to int
+	System.out.println(c);
+	System.out.println(d);
 		
-		double c = 9.78d;
-		int d = (int) c; 		//Manual casting: double to int
-		System.out.println(c);
-		System.out.println(d);
-		
-		int maxScore = 500;
-		int userScore = 423;
-		float percentage = (float) userScore / maxScore * 100.0f;
-		System.out.println("User's percentage is " + percentage);
+	int maxScore = 500;
+	int userScore = 423;
+	float percentage = (float) userScore / maxScore * 100.0f;
+	System.out.println("User's percentage is " + percentage);
 	}
 }
