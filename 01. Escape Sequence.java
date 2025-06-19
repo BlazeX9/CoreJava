4abbab7b42a198ff82180276fad58eb57e857999
Abhik <chatterjeeabhik94@gmail.com>
2025-06-19 13:36:16 +0530

    +

--- 01. Escape Sequence.java
+++ 01. Escape Sequence.java
@@ -1,18 +1,16 @@
-[Notes]
-\t    --> space
-\n    --> next line
-\' \' --> ' '
-\" \" --> " "
-
-class MyClass
-{
- public static void main(String[] args)
- {		
-  System.out.println("1)\"Hello\"\t\'World\'");  --> 1)"Hello" 'World'
-  System.out.println("2)\'Hello\'\t\"World\"");  --> 2)'Hello' "World"
-  System.out.println("3)\"Hello\"\n\'World\'");
-  System.out.println("4)\'Hello\'\n\"World\"");
-  System.out.println("5)\"Amity\"\t\'Computar\'\tAcademy");
-  System.out.println("6)Name\tRoll\tCourse\tMarks\tGrade\nRam\t1\tC\t90\tAA\nShyam\t2\tc++\t80\tA+");
- }
+public class MyJavaClass {
+    public static void main(String[] args) {
+     
+		  \t --> Space
+		  System.out.println("Hello\tJava\tProgrammers");
+		
+		  \n --> Next line
+		  System.out.println("London\nParis");
+		
+		  '' --> \'  \'
+		  System.out.println("\'Japan\'");
+		
+		  "" --> \"  \"
+		  System.out.println("\"India\"");
+	 }
 }
