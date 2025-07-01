0393d189d69ce78d58dbf52431d748d5ca181a5c
Abhik <chatterjeeabhik94@gmail.com>
2025-07-01 19:36:39 +0530

    +

--- 00. Index.java
+++ 00. Index.java
@@ -5,14 +5,3 @@ Java is compiled into the bytecode and then its interpreted into machine code
 
 To complie --> javac filename.javac
 To run --> java className
-
-++a (pre-increment) --> Increments the value of a by 1 and returns the new value of a after incrementing
-	int a=5;
-        int b=++a;  --> a becomes 6 and b becomes 6
-
-a++ (post-increment) --> Increments the value of a by 1 and returns the original value of a before incrementing
-        int a=5;
-        int b=a++;   --> a becomes 6 and b becomes 5
-
-import java.util.Scanner;  --> To use user input from scanner
-import java.lang.Math;	   --> To do mathematical operations
