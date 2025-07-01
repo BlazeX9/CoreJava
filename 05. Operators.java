03e4ae3eb1366c781cac29fe8eb43551722c96ae
Abhik <chatterjeeabhik94@gmail.com>
2025-07-02 02:22:03 +0530

    +

--- 05. Operators.java
+++ 05. Operators.java
@@ -7,28 +7,28 @@ public class MyClass {
 	int res=num1%num2;
 	System.out.println("Remainder: "+res);
 		
-	// x+=3 > x=x+3
-	int num3=5;
-	int num4=3;
-	num3+=num4;		//num3=num3+num4
-	System.out.println("Output: "+num3);
+	// x+=y > x=x+y
+	int num1=5;
+	int num2=3;
+	num1+=num2;
+	System.out.println("Output: "+num1);
 		
-	// x-=3 > x=x-3
-	// x*=3 > x=x*3
-	// x/=3 > x=x/3
-	// x%=3 > x=x%3
+	// x-=y > x=x-y
+	// x*=y > x=x*y
+	// x/=y > x=x/y
+	// x%=y > x=x%y
 		
 	// ++a(pre-increment) --> Increments the value of a by 1 and returns the new value of a after incrementing
-	int a1=5;
-	int b1=++a1;	//a becomes 6 and b becomes 6
-	System.out.println("a1:"+a1);
-	System.out.println("b1:"+b1);
+	int a=5;
+	int b=++a;
+	System.out.println("a:"+a);   //a becomes 6
+	System.out.println("b:"+b);   //b becomes 6
 
 	// a++(post-increment) --> Increments the value of a by 1 and returns the original value of a before incrementing
-	int a2=5;
-	int b2=a2++;	//a becomes 6 and b becomes 5
-	System.out.println("a2:"+a2);
-	System.out.println("b2:"+b2);
+	int a=5;
+	int b=a++;
+	System.out.println("a:"+a);   //a becomes 6
+	System.out.println("b:"+b);   //b becomes 5
 		
 	int a=6;
 	int b=7;
@@ -44,28 +44,31 @@ public class MyClass {
 	System.out.println("Equal? "+res5);
 	boolean res6=(a!=b);
 	System.out.println("Not Equal? "+res6);
+
+	//Ternary Operator
+	(Condition)?"If true":"If false"    
 		
 	//Check a number is positive or negative using ternary operator
-	int val1=-7;
-	String opt1=(val1>0)?"Positve":"Negative";
-	System.out.println(opt1);
+	int val=-7;
+	String opt=(val>0)?"Positve":"Negative";
+	System.out.println(opt);
 		
 	//Check a number is even or odd using ternary operator
-	int val2=10;
-	String opt2=(val2%2==0)?"Even":"Odd";
-	System.out.println(opt2);
+	int val=10;
+	String opt=(val%2==0)?"Even":"Odd";
+	System.out.println(opt);
 		
 	//Find the largest number between two numbers using ternary operator
-	int val3=5;
-	int val4=7;
-	int opt3=(val3>val4)?val3:val4;
-	System.out.println("Largest Number is: "+opt3);
+	int val1=5;
+	int val2=7;
+	int opt=(val1>val2)?val1:val2;
+	System.out.println("Largest Number is: "+opt);
 		
 	//Find the largest number between three numbers using ternary operator
-	int val5=5;
-	int val6=9;
-	int val7=3;
-	int opt4=(val5>val6 && val5>val7)?val5:((val6>val7)?val6:val7);
-	System.out.println("Largest Number is: "+opt4);
+	int val1=5;
+	int val2=9;
+	int val3=3;
+	int opt=(val1>val2 && val1>val3)?val1:((val2>val3)?val2:val3);
+	System.out.println("Largest Number is: "+opt);
 	}
 }
