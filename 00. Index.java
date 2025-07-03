68235e657083144026c71dec161b6552693d7deb
Abhik <chatterjeeabhik94@gmail.com>
2025-07-04 01:49:05 +0530

    Update 00. Index.java

--- 00. Index.java
+++ 00. Index.java
@@ -1,7 +1,25 @@
-Java is an object oriented programming language
+What is Java?
+- Java is an object oriented programming language. Java complies source code into the bytecode(.class file) and then its interpreted into machine readable code.
 
-Java is compiled into the bytecode and then its interpreted into machine code
-[Source code] --Compiled--> [Byte code] --Interpreted--> [Machine code]
+Is Java Platform Independent?
+- Yes, Java is a Platform Independent language. This .class file is independent of the software or hardware but needs a JVM preinstalled in the operating system for execution. Although JVM is platform dependent the bytecode can be created on any System and can be executed in any other system which makes Java platform independent.
 
-To complie --> javac filename.javac
-To run --> java className
+JRE (Java Runtime Environment)
+- JRE is an installation package that creates an environment to run the Java program or application on any machine.  
+
+JDK (Java Development Kit)
+- JDK creates the environment for developing Java applications.
+
+JVM (Java Virtual Machine)
+- JVM is part of JRE, JVM is a interpreter converts bytecode(.class file) into machine readable code.
+
+JRE vs JDK
+- JDK creates an environment to develop Java programs and JRE to execute Java programs.
+
+What is variables?
+- Variables are containers for storing data values.
+
+What is typecasting?
+- Typecasting is the process of converting a variable from one type to another. In Java there are two types of casting:
+  1. Implicit: automatic type conversion from a smaller to a larger type. Ex - int to double
+  2. Explicit: manual type conversion converts a larger type to a smaller type. Ex- double to int
