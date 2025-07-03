c96ac5571d6fb50e372413ed3172d3066d485be6
Abhik <chatterjeeabhik94@gmail.com>
2025-07-04 04:18:07 +0530

    Update 12. Function.java

--- 12. Function.java
+++ 12. Function.java
@@ -1,1 +1,14 @@
+public class NRNA {
+	public static void main(String[] args) {
+		//NRNA--->No Return No Argument
+		NRNA obj = new NRNA();
+		obj.sum();
+	}
+	void sum() {
+		int a = 5;
+		int b = 7;
+		int res = a+b;
+		System.out.println("Sum: "+res);
+	}
+}
 
