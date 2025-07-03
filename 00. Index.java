53322e5c5454d1e9becb7002217e76e61dff98a2
Abhik <chatterjeeabhik94@gmail.com>
2025-07-04 01:58:31 +0530

    Update 00. Index.java

--- 00. Index.java
+++ 00. Index.java
@@ -23,3 +23,19 @@ What is typecasting?
 - Typecasting is the process of converting a variable from one type to another. In Java there are two types of casting:
   1. Implicit: automatic type conversion from a smaller to a larger type. Ex - int to double
   2. Explicit: manual type conversion converts a larger type to a smaller type. Ex- double to int
+
+Array
+- An array is a container that holds a fixed number of values of a single type.
+  int[] myIntArray = new int[10];
+  String[] myStringArray = new String[50];
+
+What are literals?
+-   
+
+What is a constructor?
+- A constructor is a block of code similar to a method but unlike methods constructors have no return type and have the same name as the class itself.  
+
+
+
+
+  
