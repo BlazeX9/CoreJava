013dbe38ac2a2e06430403df7acdfd2eea575aa3
Abhik <chatterjeeabhik94@gmail.com>
2026-07-16 20:28:38 +0530

    Update super keyword.java

--- super keyword.java
+++ super keyword.java
@@ -1,101 +1,55 @@
-Note: super keyword is used in inheritance to access the value of a parent class in a child class where both having function name.
+Note: The super keyword is used in inheritance to access the parent class's methods, variables, and constructors. It is commonly used when the child class has a method or variable with the same name as the parent class. 
+When the parent and child classes have methods with the same name and parameters, the child class method overrides the parent class method. The super keyword can be used to call the overridden method from the parent class.	
+The super keyword can also be used to call the parameterized constructor of the parent class by passing values to it.
 
-1. super keyword in variable
-----------------------------
-class MyClass
-{
-	public static void main(String[] args)
-	{
-	 ChildClass obj=new ChildClass();
-	 obj.show1();
-	 obj.show2();
-	 obj.show3();
-	}
-}
-
-class GrandClass
-{
-	int a=100;
-	void show1()
-	{
-	 System.out.println("Grand Parent class output "+a);
-	}
-}
-class ParentClass extends GrandClass
-{
-	int a=70;
-	void show2()
-	{
-	 System.out.println("Parent class output "+a+" "+super.a);
-	}
-}
-class ChildClass extends ParentClass
-{
-	int a=50;
-	void show3()
-	{
-	 System.out.println("Child class output "+a+" "+super.a);
-	}
-}
-
-2. super keyword in method
---------------------------
-class MyClass
-{
-	public static void main(String[] args)
-	{
-	 ClassC obj=new ClassC();
-         obj.show();
-	}
-}
-
-class ClassP
-{
-	void show()
-	{
+	
+Example:
+--------
+class ClassP {
+	void show() {
 	 System.out.println("Hello");
 	}
 }
-class ClassC extends ClassP
-{
-	void show()
-	{
+
+class ClassC extends ClassP {
+	void show() {
 	 super.show();
 	 System.out.println("World");
 	}
 }
 
-3. super keyword in constructor
--------------------------------
-class MyClass
-{
-	public static void main(String[] args)
-	{
-	 ClassX obj1=new ClassX();
-	 ClassX obj2=new ClassX(10);
+class MyClass {
+	public static void main(String[] args) {
+	 ClassC obj=new ClassC();
+         obj.show();
 	}
 }
 
-class ClassY
-{
-	ClassY()
-	{
-	 System.out.println("ClassY");		//[Note: parent class non parametazied constructor is called by default]
+
+Example:
+-------------------------------
+class ClassY {
+	ClassY() {
+	 System.out.println("ClassY");
 	}
-	ClassY(int a)
-	{
+	ClassY(int a) {
 	 System.out.println("ClassYY "+a);
 	}
 }
-class ClassX extends ClassY
-{
-	ClassX()
-	{
+
+class ClassX extends ClassY {
+	ClassX() {
 	 System.out.println("ClassX");		
 	}
-	ClassX(int a)
-	{
+	ClassX(int a) {
 	 super(30);
 	 System.out.println("ClassXX "+a);
+	}
+}
+
+class MyClass {
+	public static void main(String[] args) {
+	 ClassX obj1=new ClassX();
+	 ClassX obj2=new ClassX(10);
 	}
 }
