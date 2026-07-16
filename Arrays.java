4320ff5aa444c3a2772acb6203f093b3c8978b18
Abhik <chatterjeeabhik94@gmail.com>
2026-07-16 21:47:40 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -3,9 +3,11 @@ int [] marks = new int[5];
 int [] marks = {10,20,30,40,50};
 
 #### To print the array values
+```java
 for(int i=0;i<marks.length;i++) {
   System.out.println(marks[i]);
 }
+```
 
 #### To print the array values in reverse order
 for(int i=marks.length -1;i>=0;i--) {
