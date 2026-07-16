e78eab8616db2821810e5f44e2fcbad8c805c168
Abhik <chatterjeeabhik94@gmail.com>
2026-07-16 21:50:10 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,30 +1,18 @@
-## Array Declaration
-
-```java
+// Array Declaration
 int[] marks = new int[5];
 int[] marks = {10, 20, 30, 40, 50};
-```
 
-### To print the array values
-
-```java
+// To print the array values
 for (int i = 0; i < marks.length; i++) {
     System.out.println(marks[i]);
 }
-```
 
-### To print the array values in reverse order
-
-```java
+// To print the array values in reverse order
 for (int i = marks.length - 1; i >= 0; i--) {
     System.out.println(marks[i]);
 }
-```
 
-### To print the array values using a for-each loop
-
-```java
+// To print the array values using a for-each loop
 for (int element : marks) {
     System.out.println(element);
 }
-```
