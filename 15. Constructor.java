a3be716d9c40a9bee54fd4ded16c76abfd51b940
Abhik <chatterjeeabhik94@gmail.com>
2026-07-22 22:56:37 +0530

    Update 15. Constructor.java

--- 15. Constructor.java
+++ 15. Constructor.java
@@ -1,7 +1,6 @@
 //A Java constructor is a method that is invoked automatically when an object of a class is created.
 //The constructor name must be exactly identical to the class name
 //A constructor does not have any return type, not even void
-
 class MyClass {
     MyClass() {
         System.out.println("Hello World");
