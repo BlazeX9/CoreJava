0de8a6760bd81d719ccdf024c4094baf9f108ab4
Abhik <chatterjeeabhik94@gmail.com>
2026-07-30 14:54:31 +0530

    30-07-2026

---
+++ 9. OneDimentionalArray.java
@@ -0,0 +1,17 @@
+int[] marks = new int[5];
+int[] marks = {10, 20, 30, 40, 50};
+
+// To print the array values
+for (int i=0; i<marks.length; i++) {
+    System.out.println(marks[i]);
+}
+
+// To print the array values in reverse order
+for (int i=marks.length -1; i>=0; i--) {
+    System.out.println(marks[i]);
+}
+
+// To print the array values using a for-each loop
+for (int value:marks) {
+    System.out.println(value);
+}
