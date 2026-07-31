6ae5d087d28d3c32fac767447b528d0a16fea79b
Abhik <chatterjeeabhik94@gmail.com>
2026-07-31 12:40:28 +0530

    31-07-2026

--- 19. Super Keyword.java
+++ 19. Super Keyword.java
@@ -18,3 +18,34 @@ public class JavaSuper {
         obj.show();
     }
 }
+
+
+//constructor overriding
+class ParentClassX {
+	ParentClassX(int a) {
+        int res = a+10;
+	    System.out.println("Values of a = "+res);
+	}
+	ParentClassX(int a,int b) {
+        int res = a+b;
+	    System.out.println("Values of b = "+b);
+        System.out.println("Total is = "+res);
+	}
+    void show() {
+        System.out.println("");
+    }
+}
+
+class ChildClassX extends ParentClassX {
+	ChildClassX(int a) {
+        super(a,15);
+	    System.out.println("Values of a = "+a);		
+	}
+}
+
+public class JavaSuper2 {
+    public static void main(String[] args) {
+        ChildClassX obj = new ChildClassX(10);
+        obj.show();
+    }
+}
