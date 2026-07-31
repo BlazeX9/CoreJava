76685268f7b9901ed6bc3e0866a2afeb9930514f
Abhik <chatterjeeabhik94@gmail.com>
2026-07-31 11:58:57 +0530

    31-07-2026

--- 02. DataTypes.java
+++ 02. DataTypes.java
@@ -1,9 +1,9 @@
-#Rules for declaring a variable name:-
-1. Must not begin with a digit
-2. Blank spaces are not allowed
-3. Name can't be a reserved keyword
-4. Can contain characters like $ _
-5. Variable name is case sensitive
+//Rules for declaring a variable name:-
+//1. Must not begin with a digit
+//2. Blank spaces are not allowed
+//3. Name can't be a reserved keyword
+//4. Can contain characters like $ _
+//5. Variable name is case sensitive
 
 public class MyClass {
     public static void main(String[] args) {
@@ -11,10 +11,10 @@ public class MyClass {
 	int a = 7;
 	System.out.println(a);
 
-	long b = 1234567890123456789l;	//l
+	long b = 1234567890123456789l;
 	System.out.println(b);
 
-	float c = 3.14f;		//f
+	float c = 3.14f;
 	System.out.println(c);
 
 	double d = 3.14159265359;
