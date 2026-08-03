80657fcbd54476ddaea5aef7198b33cac4a190a3
Abhik <chatterjeeabhik94@gmail.com>
2026-08-03 21:10:47 +0530

    03-08-2026

--- 23. StaticBlock.java
+++ 23. StaticBlock.java
@@ -1,9 +1,10 @@
+//A static block is a block of code that executes once when the class is loaded before the main() method
 class SubClass {
-	void show() {
-	 	System.out.println("Sub class method");
-	}
 	static {
 	 	System.out.println("Sub class static block");
+	}
+	void show() {
+	 	System.out.println("Sub class method");
 	}
 }
 
@@ -12,6 +13,7 @@ public class StaticBlock {
 	 	System.out.println("Main class static block");
 	}
 	public static void main(String[] args) {
+		System.out.println("Main class method");
 		SubClass obj=new SubClass();
 		obj.show();
 	}
