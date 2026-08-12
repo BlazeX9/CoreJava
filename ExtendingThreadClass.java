36d5f5de7fc60e8a7074f4d23b77caf35a5afea1
Abhik <chatterjeeabhik94@gmail.com>
2026-08-12 11:36:45 +0530

    Add files via upload

---
+++ ExtendingThreadClass.java
@@ -0,0 +1,25 @@
+//Multithreading enables a Java program to run multiple threads simultaneously, allowing tasks to execute in parallel
+class myThreadOne extends Thread {
+    public void run() {
+        for(int i=0;i<100;i++) {
+            System.out.println("Thread One "+i);
+        }
+    }
+}
+
+class myThreadTwo extends Thread {
+    public void run() {
+        for(int i=0;i<100;i++) {
+            System.out.println("Thread Two "+i);
+        }
+    }
+}
+
+public class ExtendingThreadClass {
+    public static void main(String[] args) {
+        myThreadOne t1 = new myThreadOne();
+        myThreadTwo t2 = new myThreadTwo();
+        t1.start();
+        t2.start();
+    }
+}
